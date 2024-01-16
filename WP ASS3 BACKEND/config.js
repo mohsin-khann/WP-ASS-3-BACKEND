bff99bdad185398f67a7c26d2fa3635728addd4f
mohsin-khann <147134483+mohsin-khann@users.noreply.github.com>
2024-01-16 06:10:14 -0800

    Add files via upload
    ecomerce backend assighnment

---
+++ WP ASS3 BACKEND/config.js
@@ -0,0 +1,6 @@
+export const PORT = 3000;
+
+export const mongoDBURL =  'mongodb+srv://mohsinafridi:mohsin555@cluster2.2kvlk5m.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
